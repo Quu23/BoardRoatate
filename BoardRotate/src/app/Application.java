d3f4daf13cbb5f41f43177809fc9b014021f4086
Quu23 <kiyu@kaz.bbiq.jp>
2025-04-25 19:41:54 +0900

    すこし修正

--- BoardRotate/src/app/Application.java
+++ BoardRotate/src/app/Application.java
@@ -6,7 +6,6 @@ import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Stack;
 
 import javax.swing.SwingUtilities;
